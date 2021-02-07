d501d7132ceb3b771166f47e8c71eca8fe99aeda
Hyperion <pvega.impulsa@gmail.com>
2021-02-07 12:19:33 +0100

    biblioteca v1

---
+++ catalog/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    soledad_books = Book.objects.filter(title__contains='soledad')
+    number_soledad_books = soledad_books.count()
+
+    # Will match on: Fiction, Science fiction, non-fiction etc.
+    books_conteaining_genre = Book.objects.filter(genre__name__icontains='ficcion')
+    number_genre = books_conteaining_genre.count()
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'number_soledad_books':number_soledad_books, 'number_genre':number_genre, 'num_visits':num_visits},
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
